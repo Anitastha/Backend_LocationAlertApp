903bcebbb0ca1132f7fdf845b1d4303f0520b318
Anita <taniastha@gmail.com>
2019-12-08 22:42:32 +0545

    final changes

--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,5 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var User = require('./models/users');
 
-passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
